1718308000e28e77ca3fde60e3ab5c0182e819a4
Anjali Patel <anjlip2112@gmail.com>
2021-03-27 14:50:54 +0530

    basic code created

---
+++ basics.py
@@ -0,0 +1,20 @@
+# adding two numbers
+a = 50
+b = 20
+c = a+b
+print(c)
+
+# without storing the sum in third variable
+a = 50
+b = 20
+print(a+b)
+
+# similary other operations can be performed
+print(a*b)
+print(a-b)
+print(a%b)
+
+# taking values from a user
+x = int(input("Enter the value of x:"))    # using int() because we want integer type value from the user
+y = int(input("Enter the value of y:"))
+print(x+y)
