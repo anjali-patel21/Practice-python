664840469fb033803516b19e9923ca2aec53ddfd
Anjali Patel <anjlip2112@gmail.com>
2021-04-13 19:00:51 +0530

    Update exception-handling.py

--- exception-handling.py
+++ exception-handling.py
@@ -7,8 +7,15 @@ try:
     print("Execution started")
     print(a/b)
 
-except Exception:                           # except block will only execute when error occurs
+except ZeroDivisionError as e:                           # except block will only execute when error occurs
     print("You cannot divide any number by zero")
+
+try:
+    k = int(input("Enter value for k:"))
+    print(k)
+
+except ValueError as e:
+    print("You need to enter integer value!!!")
 
 finally:                                    # finally block will always execute
     print("Execution ended")
